cc449864d3ec345cab30e413a2f08fc15ecd8745
kkevinsm <76790044+kkevinsm@users.noreply.github.com>
2024-11-30 01:24:28 +0700

    tab voters done

--- resources/views/layouts/footers/auth/footer.blade.php
+++ resources/views/layouts/footers/auth/footer.blade.php
@@ -4,7 +4,7 @@
         <div class="row align-items-center justify-content-lg-between">
             <div class="col-lg-6 mb-lg-0 mb-4">
                 <div class="copyright text-center text-sm text-muted text-lg-start">
-                    © <script>
+                    Copyright © <script>
                         document.write(new Date().getFullYear())
                     </script>, made with ❤️ by
                     <a href="https://www.hangker-sepanjang.com" class="font-weight-bold" target="_blank">Hangker Sepanjang</a>
