ae47f375c8c3b0e1529ef66bb79c2844a8de8843
IshlahYT <ishlah.kiky@gmail.com>
2024-12-01 17:05:45 +0700

    dashboard

--- resources/views/admin/profile/index.blade.php
+++ resources/views/admin/profile/index.blade.php
@@ -12,9 +12,6 @@
                 <div class="col-auto">
                     <div class="avatar avatar-xl position-relative">
                         <img src="{{ asset('assets/img/bruce-mars.jpg') }}" alt="..." class="w-100 border-radius-lg shadow-sm">
-                        <a href="javascript:;" class="btn btn-sm btn-icon-only bg-gradient-light position-absolute bottom-0 end-0 mb-n2 me-n2" data-bs-toggle="tooltip" data-bs-placement="top" title="Edit Image">
-                            <i class="fa fa-pen top-0"></i>
-                        </a>
                     </div>
                 </div>
                 <div class="col-auto my-auto">
